0ae5e32c6414873834ddd619d15fb7b89e5d1e71
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-08-19 05:12:53 +0530

    Update Calculation.java

--- 18th August Lab/Calculation.java
+++ 18th August Lab/Calculation.java
@@ -16,7 +16,7 @@ public class Calculation {
 		   System.out.printf("The Difference of two integers: %d%n ",firstInt-SecondInt);
 		   System.out.printf("The Product of two integers: %d%n ",firstInt*SecondInt);
 		   System.out.printf("The Average of two integers: %.2f%n",(double)(firstInt+SecondInt)/2);
-           System.out.printf("The Distance of two integers: %d%n",Math.abs(firstInt-SecondInt)); 	
+                   System.out.printf("The Distance of two integers: %d%n",Math.abs(firstInt-SecondInt)); 	
 		   System.out.printf("The Max integer: %d%n",Math.max(firstInt,SecondInt));
 		   System.out.printf("The Min integer: %d%n",Math.min(firstInt,SecondInt));
 				
